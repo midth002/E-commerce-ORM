b7a169811bfc84d714576f8dca663669668ee54b
Andrew Midthun <midth002@gmail.com>
2022-05-01 11:38:53 -0500

    Added functionality t othe product route on delete from req.params.id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,8 +118,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where : {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteProduct) { 
+      res.status(404).json({ message : 'There is no Product with this ID in the Database.'}); 
+      return;
+    }
+
+    res.status(200).json(deleteProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 });
 
 module.exports = router;
