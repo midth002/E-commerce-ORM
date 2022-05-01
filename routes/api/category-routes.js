3343e8b9fe002090fa978157df10e3b030ec00c0
Andrew Midthun <midth002@gmail.com>
2022-05-01 10:26:05 -0500

    Added functionality to the category-routes.js put route for updating categories by id in the req.params section of the url.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,41 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create({
+    category_name : req.body.category_name
+  });
+  res.status(200).json(createCategory);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update(req.body, {
+      where : {
+        id: req.params.id
+      }
+    }); 
+
+    // If the user has a unknown id in the req.params.id section in the url. Send back a 400 bad request error that there is no category with that ID in the db.
+    if (!catData[0]) {
+      res.status(400).json({ message: 'No category data found for this ID in our database' });
+      return; 
+    }
+
+    res.status(200).json({ message: `Updated category ${catData}`})
+
+  }
+    catch (error) {
+      res.status(500).json(error)
+    }
+  
+
 });
 
 router.delete('/:id', (req, res) => {
