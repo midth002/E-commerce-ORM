4a576dad211f90a75e15ec599c9d8a15ce696ece
Andrew Midthun <midth002@gmail.com>
2022-05-01 11:28:39 -0500

    Added functionality to product routes get route by id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,27 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include : 
+      [{ model : Category }, { model : Tag }]
+    });
+
+    if(!productData) {
+      res.status(404).json({ message : 'No product found in the Database with this product ID.'});
+      return;
+    }
+
+    res.status(200).json(productData);
+
+  }
+    catch (error) {
+      res.status(200).json(error);
+    }
+ 
 });
 
 // create new product
